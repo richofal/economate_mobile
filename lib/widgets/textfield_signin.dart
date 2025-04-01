511fc74a5dc35c07845a02d0e4c2d66a7af49da2
Taraka2027 <mhtaraka2027@gmail.com>
2025-04-01 18:15:54 +0700

    Memambahkan file widgets textfield sign in

---
+++ lib/widgets/textfield_signin.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextfieldSignin extends StatelessWidget{
+  final String isLabel;
+
+  const TextfieldSignin({super.key, required this.isLabel});
+
+  @override
+  Widget build(BuildContext context) {
+    String icons = "assets/svgs/${isLabel.toLowerCase()}.svg";
+    // TODO: implement build
+    return SizedBox(
+              width: 300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  // Stack untuk menumpuk Container lingkaran di depan TextField
+                  Stack(
+                    clipBehavior: Clip.none,  // Membuat agar lingkaran bisa melampaui batas Row
+                    children: [
+                      Container(
+                        height: 42,
+                        width: 300, // Menentukan lebar TextField
+                        padding: const EdgeInsets.only(left: 70),
+                        decoration: BoxDecoration(
+                          color: Color(0xFFFBFBFB),
+                          borderRadius: BorderRadius.circular(30.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xB1C5BAFF),
+                              spreadRadius: 1,
+                              blurRadius: 8,
+                            ),
+                          ],
+                        ),
+                        child: TextField(
+                          style: GoogleFonts.plusJakartaSans(
+                            color: Color(0xFF767676),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          obscureText: true,
+                          obscuringCharacter: "*",
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            hintStyle: GoogleFonts.plusJakartaSans(
+                              color: Color(0xFF767676),
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(vertical: 11.0),
+                          ),
+                        ),
+                      ),
+                      // Container lingkaran yang ditumpuk di depan TextField
+                      Positioned(
+                        top: -4,   // Menempatkan lingkaran sedikit di atas
+                        child: Container(
+                          height: 50,
+                          width: 50,
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFFBFBFB),
+                            borderRadius: BorderRadius.circular(30.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0xB1C5BAFF),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: SvgPicture.asset(
+                            icons,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+  }
+}  
