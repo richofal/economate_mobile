b146354108fd912f960d82f802614e352d1ca646
Taraka2027 <mhtaraka2027@gmail.com>
2025-04-02 11:52:18 +0700

    Menambahkan widget saldo besar

---
+++ lib/widgets/saldobesar_home.dart
@@ -0,0 +1,51 @@
+import 'package:economate_mobile/constants/color_constant.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
+
+class SaldobesarHome extends StatelessWidget{
+  final String type;
+  final String nominal;
+
+  const SaldobesarHome({super.key, required this.type, required this.nominal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Saldo',
+          style: GoogleFonts.plusJakartaSans(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: ColorConstant.putih
+          ),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Rp',
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 21,
+                fontWeight: FontWeight.w600,
+                color: ColorConstant.putih
+              ),
+            ),
+
+            const Gap(5),
+
+            Text('20.274.714',
+              style: GoogleFonts.plusJakartaSans(
+                fontSize: 40,
+                fontWeight: FontWeight.w700,
+                color: ColorConstant.putih
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
