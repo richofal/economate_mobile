07d48fe81400b70d7e1daebf60a972931ae99572
Taraka2027 <mhtaraka2027@gmail.com>
2025-04-15 22:30:57 +0700

    Fiksasi untuk pengumpulan UTS

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -23,40 +23,42 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorConstant.birumuda,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0 ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Center secara vertikal
-            crossAxisAlignment: CrossAxisAlignment.center, // Center secara horizontal
-            children: [
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  style: TextStyle(
-                    fontFamily: 'PlusJakartaSans', // Custom font
-                    fontSize: 18,
-                    color: Colors.white, // Warna font putih
-                  ),
-                  children: [
-                    TextSpan(text: 'Aplikasi yang siap jadi '), // Normal
-                    TextSpan(
-                      text: 'teman', // Bold
-                      style: TextStyle(fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0 ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Center secara vertikal
+              crossAxisAlignment: CrossAxisAlignment.center, // Center secara horizontal
+              children: [
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: TextStyle(
+                      fontFamily: 'PlusJakartaSans', // Custom font
+                      fontSize: 18,
+                      color: Colors.white, // Warna font putih
                     ),
-                    TextSpan(text: ' dalam mengelola keuangan pribadi'),
-                  ],
+                    children: [
+                      TextSpan(text: 'Aplikasi yang siap jadi '), // Normal
+                      TextSpan(
+                        text: 'teman', // Bold
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      TextSpan(text: ' dalam mengelola keuangan pribadi'),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 50), // Jarak antara teks dan logo
-              // Logo
-              Image.asset(
-                'assets/images/logo.png',
-                width: 200,
-                height: 200,
-                fit: BoxFit.contain,
-              ),
-            ],
+                SizedBox(height: 50), // Jarak antara teks dan logo
+                // Logo
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
           ),
         ),
       ),
